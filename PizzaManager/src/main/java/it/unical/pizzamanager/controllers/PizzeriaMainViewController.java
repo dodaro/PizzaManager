635f57a9c84c065bad72208cae6fedaa4d3a0230
fracos23 <francesco@francesco-S551LN>
2016-01-09 17:09:32 +0100

    PizzeriaMainViewController

---
+++ PizzaManager/src/main/java/it/unical/pizzamanager/controllers/PizzeriaMainViewController.java
@@ -0,0 +1,16 @@
+package it.unical.pizzamanager.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PizzeriaMainViewController {
+
+	// private static final Logger logger = LoggerFactory.getLogger(MapsController.class);
+
+	@RequestMapping(value = "/pizzeriamainview", method = RequestMethod.GET)
+	public String home() {
+		return "pizzeriamainview";
+	}
+}
