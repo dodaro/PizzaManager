34e661c561cbc56f8a2fc8cb26195e4369953f19
fracos23 <fracos23@gmail.com>
2016-01-20 13:12:23 +0100

    Added PizzaUtils (mi serviva un piccolo supporto), modificata home user per mostrare le top 5 pizze, added query a orderItem

---
+++ PizzaManager/src/main/java/it/unical/pizzamanager/utils/PizzaUtils.java
@@ -0,0 +1,37 @@
+package it.unical.pizzamanager.utils;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import it.unical.pizzamanager.persistence.dto.Pizza;
+
+public class PizzaUtils {
+
+	private Integer numberOccurrence;
+	private Pizza pizza;
+	
+	public int getNumberOccurrence() {
+		return numberOccurrence;
+	}
+
+	public void setNumberOccurrence(Integer numberOccurrence) {
+		this.numberOccurrence = numberOccurrence;
+	}
+
+	public Pizza getPizza() {
+		return pizza;
+	}
+
+	public void setPizza(Pizza pizza) {
+		this.pizza = pizza;
+	}
+
+	public PizzaUtils(Integer numberOccurrence, Pizza pizza) {
+		super();
+		this.numberOccurrence = numberOccurrence;
+		this.pizza = pizza;
+	}
+	
+		
+}
