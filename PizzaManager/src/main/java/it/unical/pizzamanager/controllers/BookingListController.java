717a9fd6e546cebd44b29f9e2202d8a067526fd9
MarcoCos <cosentinomarco90@gmail.com>
2016-01-25 16:05:35 +0100

    Fixed removal of a booking

--- PizzaManager/src/main/java/it/unical/pizzamanager/controllers/BookingListController.java
+++ PizzaManager/src/main/java/it/unical/pizzamanager/controllers/BookingListController.java
@@ -19,10 +19,12 @@ import org.springframework.web.context.WebApplicationContext;
 import it.unical.pizzamanager.model.BookingUserDisplay;
 import it.unical.pizzamanager.persistence.dao.BookingDAO;
 import it.unical.pizzamanager.persistence.dao.OrderItemDAO;
+import it.unical.pizzamanager.persistence.dao.PizzeriaDAO;
 import it.unical.pizzamanager.persistence.dao.PaymentDAO;
 import it.unical.pizzamanager.persistence.dao.UserDAO;
 import it.unical.pizzamanager.persistence.dto.Booking;
 import it.unical.pizzamanager.persistence.dto.OrderItem;
+import it.unical.pizzamanager.persistence.dto.Pizzeria;
 import it.unical.pizzamanager.persistence.dto.User;
 import it.unical.pizzamanager.utils.BookingUserDisplayUtils;
 import it.unical.pizzamanager.utils.SessionUtils;
@@ -34,7 +36,7 @@ public class BookingListController {
 	WebApplicationContext context;
 
 	@RequestMapping(value = "/orders", method = RequestMethod.GET)
-	public String orders(Model model, HttpSession session,HttpServletRequest request) {
+	public String orders(Model model, HttpSession session, HttpServletRequest request) {
 		if (!SessionUtils.isUser(session)) {
 			return "index";
 		}
@@ -43,40 +45,44 @@ public class BookingListController {
 		User user = userDAO.get(SessionUtils.getUserIdFromSessionOrNull(session));
 		model.addAttribute("user", user);
 		List<Booking> bookings = bookingDAO.getActiveUserBookings(user);
-		List<BookingUserDisplay> bookingsList = createBookingList(bookings,bookingDAO);
+		List<BookingUserDisplay> bookingsList = createBookingList(bookings, bookingDAO);
 		model.addAttribute("bookings", bookingsList);
 		return "orders";
 	}
-	
-	
-	@RequestMapping(value="/orders/removeItem",method=RequestMethod.POST)
-	public String removeItem(@RequestParam int iditem,@RequestParam int idbooking,Model model,HttpSession session){
+
+	@RequestMapping(value = "/orders/removeItem", method = RequestMethod.POST)
+	public String removeItem(@RequestParam int iditem, @RequestParam int idbooking, Model model, HttpSession session) {
 		if (!SessionUtils.isUser(session)) {
 			return "index";
 		}
 		UserDAO userDAO = (UserDAO) context.getBean("userDAO");
 		BookingDAO bookingDAO = (BookingDAO) context.getBean("bookingDAO");
 		OrderItemDAO orderItemDAO = (OrderItemDAO) context.getBean("orderItemDAO");
+		PizzeriaDAO pizzeriaDAO = (PizzeriaDAO) context.getBean("pizzeriaDAO");
 		User user = userDAO.get(SessionUtils.getUserIdFromSessionOrNull(session));
-		int itemId=iditem;
-		int bookingId=idbooking;
-		Booking booking=userDAO.getBooking(bookingId,user.getId());
-		if(booking==null)
+		int itemId = iditem;
+		int bookingId = idbooking;
+		Booking booking = userDAO.getBooking(bookingId, user.getId());
+		if (booking == null)
 			return "index";
-		
+
 		for (OrderItem item : booking.getOrderItems()) {
-			if(item.getId()==itemId){
-				orderItemDAO.delete(item);
+			if (item.getId() == itemId) {
 				booking.getOrderItems().remove(item);
+				bookingDAO.update(booking);
+				orderItemDAO.delete(item);
+
+				if (booking.getOrderItems().size() == 0) {
+					bookingDAO.delete(booking);
+					model.addAttribute("user", user);
+					return "orders";
+				}
 				break;
-			}		
+			}
 		}
-		if(booking.getOrderItems().isEmpty()){
-			bookingDAO.delete(booking);
-			return "orders";
-		}
-		PaymentDAO paymentDAO=(PaymentDAO) context.getBean("paymentDAO");
-		BookingUserDisplayUtils.cancelPayment(booking,paymentDAO);
+
+		PaymentDAO paymentDAO = (PaymentDAO) context.getBean("paymentDAO");
+		BookingUserDisplayUtils.cancelPayment(booking, paymentDAO);
 		bookingDAO.update(booking);
 		model.addAttribute("user", user);
 		return "orders";
@@ -84,16 +90,14 @@ public class BookingListController {
 
 	private List<BookingUserDisplay> createBookingList(List<Booking> bookings, BookingDAO bookingDAO) {
 		List<BookingUserDisplay> bookingList = new ArrayList<>();
-	
+
 		for (Booking booking : bookings) {
-			List<Booking> activeBooking=bookingDAO.getOrderedBookings(booking.getPizzeria());
-			BookingUserDisplay bookingUserDisplay=BookingUserDisplayUtils.createBookingUserDisplay(booking,activeBooking);
+			List<Booking> activeBooking = bookingDAO.getOrderedBookings(booking.getPizzeria());
+			BookingUserDisplay bookingUserDisplay = BookingUserDisplayUtils.createBookingUserDisplay(booking,
+					activeBooking);
 			bookingList.add(bookingUserDisplay);
 		}
 		return bookingList;
 	}
 
-	
-	
-	
 }
